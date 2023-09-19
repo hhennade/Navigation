e6758af320c1d6151450e52d258ac607d1e5b4f4
Henna <henna.hannila@gmail.com>
2023-09-19 21:27:11 +0300

    valmis navigointi

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './HomeScreen'
+import SecondScreen from './SecondScreen'
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
-export default function App() {
+function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: 'Home',
+            headerTitle: 'Home',
+          }}
+        />
+        <Stack.Screen
+          name="Second"
+          component={SecondScreen}
+          options={{
+            title: 'Second',
+            headerTitle: 'Second',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default gestureHandlerRootHOC(App);
